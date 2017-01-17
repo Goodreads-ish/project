65c747390c4c88159132edb0caa3bebbb5f5a284
Patrick Woolard <patrick.k.woolard@gmail.com>
2017-01-17 14:28:26 -0600

    Initial commit

---
+++ src/main/java/goodreads/GoodreadsCloneApplication.java
@@ -0,0 +1,12 @@
+package goodreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodreadsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodreadsCloneApplication.class, args);
+	}
+}
